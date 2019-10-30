dc290856ffcbd063bfa3745447716efef263fa52
Isaac <issac2992@hotmail.com>
2019-10-29 22:53:28 -0500

    adding dojo

---
+++ SignalRSelfHost/Scripts/grid.js
@@ -0,0 +1,15 @@
+﻿var grid;
+require([
+    "dojox/grid/DataGrid",
+    "dojo/store/Memory",
+    "dojo/data/ObjectStore",
+    "dojo/domReady!"],
+    function (DataGrid, Memory, ObjectStore) {
+        /* create store here ... */
+        var dataStore = new ObjectStore({ objectStore: new Memory({ data: [] }) });
+        grid = new DataGrid({
+            id: 'Seq',
+            store: dataStore,
+        }, "grid");
+
+    });
---
+++ SignalRSelfHost/Scripts/vesselHubClient.js
@@ -0,0 +1,51 @@
+﻿$(function () {
+    //Set the hubs URL for the connection
+    $.connection.hub.url = "http://localhost:8020/signalr";
+
+    // Declare a proxy to reference the hub.
+    var cnnVessel = $.connection.vesselHub;
+
+    $.connection.hub.start(function () {
+        var group = $('#groupList').val();
+        cnnVessel.server.join(group);
+        cnnVessel.lastGroup = group;
+    });
+
+    $('#btnConenct').click(function () {
+        $('#message').text('Connecting to ' + $('#groupList').val() + ' ...');
+        setTimeout(function () {
+            if ($.connection.hub.state === 1) {
+                var group = $('#groupList').val();
+                cnnVessel.server.removeGroup(cnnVessel.lastGroup);
+                cnnVessel.server.join(group);
+                cnnVessel.lastGroup = group;
+            } else {
+                connect()
+            }
+        }, 1000)
+
+    });
+
+    function connect() {
+        $.connection.hub.start().done(function () {
+            $('#message').text('Done!');
+        });
+    }
+
+    // Create a function that the hub can call to broadcast messages.
+    cnnVessel.client.addMessage = function (message) {
+        console.log('Notification received for group ' + $('#groupList').val() + ' at time ' + new Date().toJSON());
+        var data = JSON.parse(message);
+        grid.store.objectStore.data = JSON.parse(data).Data;
+        //grid.store.query({ id: '*' }).forEach(function (item) {
+        //    debugger;
+        //    grid.store.remove(item.Seq);
+        //});
+
+        //data.Data.foEach(item => {
+        //    debugger;
+        //    grid.store.put(item);
+        //});
+
+    }
+});
